3ad83f9468590c1d862b2bd019fb396e36a1c08d
kiara.precopio <kprecopio@eldars.com.ar>
2025-07-24 20:11:01 -0300

    initial config

---
+++ src/main/java/com/dinoatlas/dinoatlasbackend/DinoatlasBackendApplication.java
@@ -0,0 +1,13 @@
+package com.dinoatlas.dinoatlasbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DinoatlasBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DinoatlasBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dinoatlas/dinoatlasbackend/DinoatlasBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dinoatlas.dinoatlasbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DinoatlasBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
