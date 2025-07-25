7143e317b9bceb26f77d3b355c1ca9895ed59406
kiara.precopio <kprecopio@eldars.com.ar>
2025-07-25 01:36:52 -0300

    first steps: connection with api from paleobiodb

--- src/main/java/com/precopio/dinoatlasbackend/DinoatlasBackendApplication.java
+++ src/main/java/com/precopio/dinoatlasbackend/DinoatlasBackendApplication.java
@@ -1,9 +1,11 @@
-package com.dinoatlas.dinoatlasbackend;
+package com.precopio.dinoatlasbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class DinoatlasBackendApplication {
 
 	public static void main(String[] args) {
--- src/test/java/com/precopio/dinoatlasbackend/DinoatlasBackendApplicationTests.java
+++ src/test/java/com/precopio/dinoatlasbackend/DinoatlasBackendApplicationTests.java
@@ -1,4 +1,4 @@
-package com.dinoatlas.dinoatlasbackend;
+package com.precopio.dinoatlasbackend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
